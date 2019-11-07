a1dc266cf7050dc9c5e5dc29b76ca6454986035b
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2019-11-07 01:42:49 +0100

    converted to React hooks from class

--- src/Map.js
+++ src/Map.js
@@ -1,31 +1,24 @@
-import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-class Map extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    viewport: {
-      width: 400, 
-      height: 400, 
-      latitude: 37.7577, 
-      longitude: -122.4376, 
-      zoom: 8
-    }
-  }
-  
-  };
-  render() {
-    return (
-      <ReactMapGL 
+const Map = () => {
+  const [viewport, setViewPort] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  })
+
+  return (
+    <ReactMapGL
       mapboxApiAccessToken={TOKEN}
-      {...this.state.viewport}
-      onViewportChange={(viewport) => this.setState({viewport})}
-      />  
-    );
-  }
-}
+      {...viewport}
+      onViewportChange={viewport => setViewPort({ ...viewport })}
+    />
+  );
+};
 
 export default Map;
